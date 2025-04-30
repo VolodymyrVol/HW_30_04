d94c0a88d475c3c99c76ba08ff157841348c03f8
Vova <lololoskafry@gmail.com>
2025-04-30 19:18:31 +0300

    task1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.writeln("Hello main")
+//task 1 
+// let fio = prompt("Введите ФИО: ");
+// let gender = prompt("Введите пол: ");
+// let age = parseInt(prompt("Введите возраст: "));
+// let mail = prompt("Введите почту:");
+
+// confirm(`ФИО: ${fio} \nПол: ${gender}\nВозраст: ${age}\ne-mail: ${mail}\n\nВсе верно?`)
+
+
