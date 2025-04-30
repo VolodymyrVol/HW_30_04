54486b751dcaf9af2041f3802b212b926811909d
Vova <lololoskafry@gmail.com>
2025-04-30 19:38:44 +0300

    task2

--- main.js
+++ main.js
@@ -6,4 +6,21 @@
 
 // confirm(`ФИО: ${fio} \nПол: ${gender}\nВозраст: ${age}\ne-mail: ${mail}\n\nВсе верно?`)
 
-
+//task2 
+let bilnum = prompt("Введите номер билета: ");
+let a1 = parseInt(bilnum[0]);
+let a2 = parseInt(bilnum[1]);
+let a3 = parseInt(bilnum[2]);
+let a4 = parseInt(bilnum[3]);
+let a5 = parseInt(bilnum[4]);
+let a6 = parseInt(bilnum[5]);
+firsth = a1+a2+a3;
+sech = a4+a5+a6;
+if (firsth == sech)
+{
+    document.writeln("True")
+}
+else
+{
+    document.writeln("false")
+}
